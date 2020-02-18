8a9839457ffa678b4a8abc7e6939bd132c40d9e8
Along777 <52094218+Along777@users.noreply.github.com>
2020-02-18 03:08:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+console.log('reading js');
+
+
+// Variables
+var about = document.querySelector('#about');
+
+
+// Pages
+about.addEventListener('click', function() {
+  document.querySelector('main').style.display = 'none';
+  document.querySelector('#AboutPage').style.display = 'block';
+});
